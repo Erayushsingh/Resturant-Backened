6aa17f3108198069f89426005d9c3daa2684c573
Erayushsingh <145897256+Erayushsingh@users.noreply.github.com>
2024-10-09 02:34:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mangoose from 'mongoose';
 
 export const connectDB=async ()=>{
-    await mangoose.connect('mongodb+srv://ayushsingh74340:Ayushcseiitm1234@cluster0.jlvgv.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0')
+    await mangoose.connect('')
     .then(console.log("DB Connected"))
 }
